30202040343acb0dd9f861eaa601af8259d1db58
aryanGajelli <argajelli@gmail.com>
2024-10-10 15:03:23 -0400

    Lab 1 plots

--- filePlotter.py
+++ filePlotter.py
@@ -3,10 +3,29 @@
 # Feel free to make any modifications/additions here
 
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import numpy as np
 # from utilities import FileReader
 import csv
-def plot_errors(filename , odom=False, laser=True):
+
+def moving_average(x, w):
+    return np.convolve(x, np.ones(w), 'valid') / w
+
+def low_pass_filter(adata: np.ndarray, bandlimit: int = 500, sampling_rate: int = 10000) -> np.ndarray:
+        # translate bandlimit from Hz to dataindex according to sampling rate and data size
+        bandlimit_index = int(bandlimit * adata.size / sampling_rate)
+    
+        fsig = np.fft.fft(adata)
+        
+        for i in range(bandlimit_index + 1, len(fsig) - bandlimit_index ):
+            fsig[i] = 0
+            
+        adata_filtered = np.fft.ifft(fsig)
+    
+        return np.real(adata_filtered)
+
+
+def plot_errors(filename , odom=False, label="line"):
     with open(filename, 'r') as file:
         csv_reader = csv.reader(file)
         headers = next(csv_reader)
@@ -21,44 +40,74 @@ def plot_errors(filename , odom=False, laser=True):
 
     if odom:
         plt.subplot(2, 1, 1)
-    for i in range(0, len(headers) - 1):
-        plt.plot(time_list, [lin[i] for lin in values], label= headers[i]+ " linear")
     
+    avg = 20
+    for i in range(0, len(headers) - 1):
+        plt.plot(time_list[:-avg+1], moving_average(np.array([lin[i] for lin in values]), avg), label= headers[i]+ " linear")
+
+    data_type = "odometery" if odom else "imu"
+    state_label = "state [meters, rad]" if odom else "linear acc. & angular vel. [m/s^2, rad/s]"
     #plt.plot([lin[0] for lin in values], [lin[1] for lin in values])
+    plt.xlabel("time [s]")
+    plt.ylabel(state_label)
     plt.legend()
+    plt.title(f"Robot {data_type} for {label}")
     plt.grid()
 
     if odom:
         # 2D Trajectory Plot
         plt.subplot(2, 1, 2)
         plt.plot([lin[0] for lin in values], [lin[1] for lin in values], label="x vs. y")
-        plt.legend()
+        plt.xlabel("x [meters]")
+        plt.ylabel("y [meters]")
+        plt.title(f"Robot trajectory for {label}")
         plt.grid()
 
+    plt.subplots_adjust(hspace=0.5, wspace=0.5)
     plt.show()
 
-def plot_laser(filename):
+def plot_laser(filename, label="line"):
+    data = []
     with open(filename, 'r') as file:
         csv_reader = csv.reader(file)
         headers = next(csv_reader)
         # plot only 1st row of laser scan data
-        scan, inc, t = next(csv_reader)
-        inc = float(inc)
-        t = float(t)
-        scan = scan.removeprefix("array('f',").removesuffix(')').strip().removeprefix('[').removesuffix(']')
-        scan = [float(a) for a in scan.split(',')]
-    theta = 0
-    x = []
-    y = []
-    for r in scan:
-        x.append(r*np.cos(theta))
-        y.append(r*np.sin(theta))
-        theta += inc
-
-    plt.scatter(x, y, marker='.')
-    plt.title(f"X-Y scatter plot of robot at {t=}")
-    plt.grid()
+        for line in csv_reader:
+            scan, inc, t = line
+            inc = float(inc)
+            t = float(t)
+            scan = scan.removeprefix("array('f',").removesuffix(')').strip().removeprefix('[').removesuffix(']')
+            scan = [float(a) for a in scan.split(',')]
+            data.append({"x": [], "y": [], "t": t})
+            theta = 0
+            
+            for r in scan:
+                data[-1]["x"].append(r*np.cos(theta))
+                data[-1]["y"].append(r*np.sin(theta))
+                theta += inc
+    
+    x = data[0]["x"]
+    y = data[0]["y"]
+    fig, ax = plt.subplots()
+    plt.plot([0], [0], marker='o', markersize=5, color="red", label="Robot")
+    scat = ax.scatter(x, y, marker='.')
+    plt.title(f"X-Y scatter plot of robot laser scan at {t=:.3}s for {label} motion")
+    plt.xlabel("x [meters]")
+    plt.ylabel("y [meters]")
     plt.legend()
+    plt.grid()
+
+
+    def update(frame):
+        x = data[frame]["x"]
+        y = data[frame]["y"]
+        t = data[frame]["t"]
+        _data = np.stack([x, y]).T
+        scat.set_offsets(_data)
+        plt.title(f"X-Y scatter plot of robot laser scan at {t=:.3}s for {label} motion")
+        return scat
+
+    ani = animation.FuncAnimation(fig=fig, func=update, frames=len(data), interval=30)
     plt.show()
 
 import argparse
@@ -78,8 +127,16 @@ if __name__=="__main__":
     print("plotting the files", args.files)
 
     filenames=args.files
+    
     for filename in filenames:
+        if "line" in filename:
+            label = "line"
+        elif "circle" in filename:
+            label = "circle"
+        elif "spiral" in filename:
+            label = "spiral"
         if args.laser:
-            plot_laser(filename)
+            plot_laser(filename, label=label)
         else:
-            plot_errors(filename, args.odom)
+            
+            plot_errors(filename, args.odom, label=label)
