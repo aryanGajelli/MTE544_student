4d78a99e62e998392c52e57bb77740ee6759385d
aryanGajelli <argajelli@gmail.com>
2024-11-13 17:32:23 -0500

    Enable/disable plots by clicking on legend

--- plot_errors.py
+++ plot_errors.py
@@ -1,58 +1,64 @@
+import argparse
 import matplotlib.pyplot as plt
 from utilities import FileReader
 
 
-
-
 def plot_errors(filename):
-    
-    headers, values=FileReader(filename).read_file()
-    
-    time_list=[]
-    
-    first_stamp=values[0][-1]
-    
+
+    headers, values = FileReader(filename).read_file()
+
+    time_list = []
+
+    first_stamp = values[0][-1]
+
     for val in values:
         time_list.append(val[-1] - first_stamp)
 
-    
-    
-    fig, axes = plt.subplots(2,1, figsize=(14,6))
-
+    fig, axes = plt.subplots(2, 1, figsize=(14, 6))
 
     axes[0].plot([lin[len(headers) - 3] for lin in values], [lin[len(headers) - 2] for lin in values])
     axes[0].set_title("state space")
     axes[0].grid()
 
-    
     axes[1].set_title("each individual state")
-    for i in range(0, len(headers) - 1):
-        axes[1].plot(time_list, [lin[i] for lin in values], label= headers[i])
-
-    axes[1].legend()
     axes[1].grid()
+    lines = []
+    for i in range(0, len(headers) - 1):
+        lines.append(axes[1].plot(time_list, [lin[i] for lin in values], label=headers[i]))
 
+    leg = axes[1].legend()
+    lined = {}
+    for legline, origline in zip(leg.get_lines(), lines):
+        legline.set_picker(5)  # 5 pts tolerance
+        lined[legline] = origline[0]
+
+    def onpick(event):
+        # on the pick event, find the orig line corresponding to the
+        # legend proxy line, and toggle the visibility
+        legline = event.artist
+        origline = lined[legline]
+        vis = not origline.get_visible()
+        origline.set_visible(vis)
+        # Change the alpha on the line in the legend so we can see what lines
+        # have been toggled
+        if vis:
+            legline.set_alpha(1.0)
+        else:
+            legline.set_alpha(0.2)
+        fig.canvas.draw()
+    fig.canvas.mpl_connect('pick_event', onpick)
     plt.show()
-    
-    
 
 
-
-
-
-import argparse
-
-if __name__=="__main__":
+if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Process some files.')
     parser.add_argument('--files', nargs='+', required=True, help='List of files to process')
-    
+
     args = parser.parse_args()
-    
+
     print("plotting the files", args.files)
 
-    filenames=args.files
+    filenames = args.files
     for filename in filenames:
         plot_errors(filename)
-
-
