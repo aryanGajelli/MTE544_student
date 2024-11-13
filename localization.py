21b2f57f53ef22a389b54e288f4660fd87dc6fc5
aryanGajelli <argajelli@gmail.com>
2024-11-13 18:14:08 -0500

    Added odom x, y

--- localization.py
+++ localization.py
@@ -27,7 +27,7 @@ odom_qos=QoSProfile(reliability=2, durability=2, history=1, depth=10)
 
 class localization(Node):
     
-    def __init__(self, type, dt, loggerName="Logs/robotPose.csv", loggerHeaders=["imu_ax", "imu_ay", "kf_ax", "kf_ay","kf_vx","kf_w","kf_x", "kf_y","stamp"]):
+    def __init__(self, type, dt, loggerName="Logs/robotPose.csv", loggerHeaders=["odom_x", "odom_y", "imu_ax", "imu_ay", "kf_ax", "kf_ay","kf_vx","kf_w","kf_x", "kf_y","stamp"]):
 
         super().__init__("localizer")
 
@@ -89,7 +89,7 @@ class localization(Node):
         self.pose=np.array([x, y, th, odom_msg.header.stamp])
 
         # TODO Part 4: log your data
-        self.loc_logger.log_values([imu_msg.linear_acceleration.x, imu_msg.linear_acceleration.y, vdot, v *w, v, w, x, y, Time.from_msg(odom_msg.header.stamp).nanoseconds / 1e9])
+        self.loc_logger.log_values([odom_msg.pose.pose.position.x, odom_msg.pose.pose.position.y, imu_msg.linear_acceleration.x, imu_msg.linear_acceleration.y, vdot, v *w, v, w, x, y, Time.from_msg(odom_msg.header.stamp).nanoseconds / 1e9])
       
     def odom_callback(self, pose_msg):
         self.pose=[ pose_msg.pose.pose.position.x,
